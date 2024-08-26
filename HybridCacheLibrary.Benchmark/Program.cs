4c71cd91b2c38272592308d4b8afe8f12d227864
Seyyid Yiğit <seyyid364@gmail.com>
2024-08-26 10:55:34 +0300

    update: Edited bechmark according to new structure

--- HybridCacheLibrary.Benchmark/Program.cs
+++ HybridCacheLibrary.Benchmark/Program.cs
@@ -1,7 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Microsoft.Extensions.Caching.Memory;
-using System.Net;
 using static HybridCacheLibrary.Benchmark.HybridCacheBenchmark;
 
 namespace HybridCacheLibrary.Benchmark
@@ -12,7 +11,8 @@ namespace HybridCacheLibrary.Benchmark
         [MemoryDiagnoser]
         public class HybridCacheVsMemoryCacheBenchmark
         {
-            private CountBasedHybridCache<int, string> _hybridCache;
+            private CountBasedHybridCache<int, string> _hybridCacheCountBased;
+            private SizeBasedHybridCache<int, string> _hybridCacheSizeBased;
             private MemoryCache _memoryCache;
             private readonly MemoryCacheEntryOptions _cacheEntryOptions = new MemoryCacheEntryOptions
             {
@@ -25,41 +25,90 @@ namespace HybridCacheLibrary.Benchmark
             [GlobalSetup]
             public void Setup()
             {
-                _hybridCache = new CountBasedHybridCache<int, string>(CacheSize);
+                _hybridCacheCountBased = new CountBasedHybridCache<int, string>(CacheSize);
+                _hybridCacheSizeBased = new SizeBasedHybridCache<int, string>(CacheSize, CacheSizeType.Megabytes); // Cache boyutunu Mbyte cinsinden belirliyoruz
                 _memoryCache = new MemoryCache(new MemoryCacheOptions());
 
                 for (int i = 0; i < CacheSize; i++)
                 {
-                    _hybridCache.Add(i, "value" + i);
-                    _memoryCache.Set(i, "value" + i, _cacheEntryOptions);
+                    var str = "value" + i;
+                    _hybridCacheCountBased.Add(i, str);
+                    _hybridCacheSizeBased.Add(i, str);
+                    _memoryCache.Set(i, str, _cacheEntryOptions);
                 }
             }
 
             [Benchmark]
-            public void AddItemsHybridCache()
+            public void AddItemsHybridCacheCountBased()
             {
                 for (int i = 0; i < CacheSize; i++)
                 {
-                    _hybridCache.Add(i, "newvalue" + i);
+                    _hybridCacheCountBased.Add(i, "newvalue" + i);
                 }
             }
 
             [Benchmark]
-            public void GetItemsHybridCache()
+            public void AddItemsHybridCacheSizeBased()
             {
                 for (int i = 0; i < CacheSize; i++)
                 {
-                    _hybridCache.Get(i);
+                    _hybridCacheSizeBased.Add(i, "newvalue" + i);
                 }
             }
 
             [Benchmark]
-            public void HybridCache_LongRunning()
+            public void AddItemsMemoryCache()
+            {
+                for (int i = 0; i < CacheSize; i++)
+                {
+                    _memoryCache.Set(i, "newvalue" + i, _cacheEntryOptions);
+                }
+            }
+
+            [Benchmark]
+            public void GetItemsHybridCacheCountBased()
+            {
+                for (int i = 0; i < CacheSize; i++)
+                {
+                    _hybridCacheCountBased.Get(i);
+                }
+            }
+
+            [Benchmark]
+            public void GetItemsHybridCacheSizeBased()
+            {
+                for (int i = 0; i < CacheSize; i++)
+                {
+                    _hybridCacheSizeBased.Get(i);
+                }
+            }
+
+            [Benchmark]
+            public void GetItemsMemoryCache()
+            {
+                for (int i = 0; i < CacheSize; i++)
+                {
+                    _memoryCache.TryGetValue(i, out _);
+                }
+            }
+
+            [Benchmark]
+            public void HybridCacheCountBased_LongRunning()
             {
                 for (int i = 0; i < 1000000; i++)
                 {
-                    _hybridCache.Add(i % CacheSize, $"Value {i}");
-                    var value = _hybridCache.Get(i % CacheSize);
+                    _hybridCacheCountBased.Add(i % CacheSize, $"Value {i}");
+                    var value = _hybridCacheCountBased.Get(i % CacheSize);
+                }
+            }
+
+            [Benchmark]
+            public void HybridCacheSizeBased_LongRunning()
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    _hybridCacheSizeBased.Add(i % CacheSize, $"Value {i}");
+                    var value = _hybridCacheSizeBased.Get(i % CacheSize);
                 }
             }
 
@@ -74,12 +123,22 @@ namespace HybridCacheLibrary.Benchmark
             }
 
             [Benchmark]
-            public void HybridCache_Concurrent_Add_Get()
+            public void HybridCacheCountBased_Concurrent_Add_Get()
             {
                 Parallel.For(0, CacheSize, i =>
                 {
-                    _hybridCache.Add(i, $"Value {i}");
-                    var value = _hybridCache.Get(i);
+                    _hybridCacheCountBased.Add(i, $"Value {i}");
+                    var value = _hybridCacheCountBased.Get(i);
+                });
+            }
+
+            [Benchmark]
+            public void HybridCacheSizeBased_Concurrent_Add_Get()
+            {
+                Parallel.For(0, CacheSize, i =>
+                {
+                    _hybridCacheSizeBased.Add(i, $"Value {i}");
+                    var value = _hybridCacheSizeBased.Get(i);
                 });
             }
 
@@ -92,62 +151,14 @@ namespace HybridCacheLibrary.Benchmark
                     var value = _memoryCache.Get(i);
                 });
             }
-
-
-            [Benchmark]
-            public void AddItemsMemoryCache()
-            {
-                for (int i = 0; i < CacheSize; i++)
-                {
-                    _memoryCache.Set(i, "newvalue" + i, _cacheEntryOptions);
-                }
-            }
-
-            [Benchmark]
-            public void GetItemsMemoryCache()
-            {
-                for (int i = 0; i < CacheSize; i++)
-                {
-                    _memoryCache.TryGetValue(i, out _);
-                }
-            }
         }
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            long beforeMemory = GC.GetTotalMemory(true);
-
-            var user = new User(1, "John Doe", 30, new Address("Istanbul", "Turkey"));
-
-            long afterMemory = GC.GetTotalMemory(true);
-
-            Console.WriteLine($"Memory used: {afterMemory - beforeMemory} bytes");
-            Console.WriteLine("Hello, World!");
-            //var summary = BenchmarkRunner.Run<HybridCacheVsMemoryCacheBenchmark>();
-
+            var summary = BenchmarkRunner.Run<HybridCacheVsMemoryCacheBenchmark>();
         }
     }
-
-    internal class User
-    {
-        public User(int id, string name, int age, Address address)
-        {
-            Id = id;
-            Name = name;
-            Age = age;
-            Address = address;
-        }
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public Address Address { get; set; }
-
-        public int[] ints { get; set; }
-
-        public List<string> strings { get; set; }
-    }
-    internal record Address(string City, string Country);
 }
